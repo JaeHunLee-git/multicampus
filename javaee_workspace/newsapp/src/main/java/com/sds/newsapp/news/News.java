38ed87d0ab64050bed712e377632add3f4f3950c
jaehunLee-git <joz526@naver.com>
2024-03-26 09:06:42 +0900

    javaee0326

--- javaee_workspace/newsapp/src/main/java/com/sds/newsapp/news/News.java
+++ javaee_workspace/newsapp/src/main/java/com/sds/newsapp/news/News.java
@@ -2,7 +2,7 @@ package com.sds.newsapp.news;
 
 /* 이 클래스는 로직을 작성하기 위함이 아닌, 오직 데이터 한건을 담기 위한 데이터 전달 객체로써
  * 정의한다...이러한 목적의 객체를 가리켜 어플리케이션 설계 분야에서는 Data Transfer Object
- * DTO라 한다
+ * DTO라 한다..
  * */
 public class News {
 	private int news_idx;
