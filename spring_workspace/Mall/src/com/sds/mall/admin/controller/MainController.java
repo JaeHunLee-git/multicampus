b37fdbb65809fee8d6314c05f8b704c31e57807d
jaehunLee-git <joz526@naver.com>
2024-04-13 21:41:19 +0900

    .

--- spring_workspace/Mall/src/com/sds/mall/admin/controller/MainController.java
+++ spring_workspace/Mall/src/com/sds/mall/admin/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //관리자모드 메인의 요청을 처리하는 하위 컨트롤러
 //개발자가 @Controller 어노테이션에 아무것도 명시하지 않으면, 해당 클래스를 인스턴스화 시키면서 
 //스프링이 자동으로 빈의 id를 소문자로 시작하는 클래스명을 할당한다..하지만, 개발자가 원하는 id가 있다면
-//value 에 지정하면 된다..
+//value 에 지정하면 된다.
 @Controller(value="adminMainController")
 public class MainController {
 	
