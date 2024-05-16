8d4e9d9efe40812625a3a51156204c5be5981c40
jaehunLee-git <joz526@naver.com>
2024-05-16 16:05:26 +0900

    스프링 클라우드 구현

--- springboot_workspace/MovieApp/src/main/java/com/sds/movieapp/MovieAppApplication.java
+++ springboot_workspace/MovieApp/src/main/java/com/sds/movieapp/MovieAppApplication.java
@@ -2,8 +2,10 @@ package com.sds.movieapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication // component-scan, @Configuration,...
+@EnableDiscoveryClient
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
