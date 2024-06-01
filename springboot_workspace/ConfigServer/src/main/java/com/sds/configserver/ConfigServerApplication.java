48e7db162e6b6c1b261edc50e2f5762afe8f6a1a
jaehunLee-git <joz526@naver.com>
2024-06-01 11:10:44 +0900

    깃헙 연동

--- springboot_workspace/ConfigServer/src/main/java/com/sds/configserver/ConfigServerApplication.java
+++ springboot_workspace/ConfigServer/src/main/java/com/sds/configserver/ConfigServerApplication.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
-
-//이 어플리케이션을 설정 정보를 제공하는 Config Server로 역할을 하겠다
-@EnableConfigServer
+@EnableConfigServer//이 어플리케이션을 설정 정보를 제공하는 Config Server로 역할을 하겠다
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
