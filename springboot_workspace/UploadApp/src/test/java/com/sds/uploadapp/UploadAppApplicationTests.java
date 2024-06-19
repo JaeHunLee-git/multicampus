b7eaea2b9135754775e69db7e503b582fb6d4456
jaehunLee-git <joz526@naver.com>
2024-06-19 09:25:44 +0900

    사지 업로드

---
+++ springboot_workspace/UploadApp/src/test/java/com/sds/uploadapp/UploadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.uploadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
